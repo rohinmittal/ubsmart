ba2ccf2d11718de492a9a800f6ada9e41640ad05
juggernaut-90 <vaibhavsinha90@gmail.com>
2015-11-19 08:05:54 -0500

    Adding Search Bar Functionality

--- application/controllers/catalog.php
+++ application/controllers/catalog.php
@@ -4,16 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Catalog extends CI_Controller {
 
 	public function index()
-	{
+	{			
 		$this->home1();//load->view('welcome_message');
 	}
 	public function home1()
 	{
 		//trying changes
-		$data['title']='Welcome to UBsMart!';
+		//$data['title']='Welcome to UBsMart!';
+		$data['searchval'] ="no_search_query";
 		$this->load->view('includes/header_loggedin');
-		$this->load->view('catalog',$data);
+		$this->load->view('catalog_v',$data);
 		$this->load->view('includes/footer');
 		print_r($this->session->userdata());//temporary. Feel free to remove once development of seller only/buyer only pages gets going!				
 	}
+	public function execute_search()
+	{
+		$data['searchval'] =$_POST['search_query'];
+		$this->load->view('includes/header_loggedin');
+		$this->load->view('catalog_v',$data);
+		$this->load->view('includes/footer');
+		
+	}
+	
 }
