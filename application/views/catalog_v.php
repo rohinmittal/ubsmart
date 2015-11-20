60ae3d0a4725540f211fa595a48b03c5cddbc793
juggernaut-90 <vaibhavsinha90@gmail.com>
2015-11-20 00:07:54 -0500

    Work on Browsing the catalog and Search Functionality

--- application/views/catalog_v.php
+++ application/views/catalog_v.php
@@ -9,8 +9,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 
 	<div id="body">
-	The page you are looking at is the homepage for UBsMart!!!!<?php echo($searchval) ?>
+	The page you are looking at is the homepage for UBsMart!!!!<?php 
+	if($searchval != "no_search_query"){echo nl2br("\nSearch results for: ".$searchval);} ?>
 	</div>
-	
-		
+	<div id="product_related">
+		<div id="filter_related">
+			<fieldset>
+				<legend>Sort by:</legend>
+				<input type="radio" name="sort" value="asc_price">Price: Low to High<br>
+				<input type="radio" name="sort" value="desc_price">Price: Low to High<br>
+				<input type="radio" name="sort" value="asc_tier">Price: Low to High<br>
+				<input type="radio" name="sort" value="desc_tier">Price: Low to High<br>
+			</fieldset>
+		</div>
+		<div id="product_grid">			
+		</div>
+	</div>		
 </div>
--- application/views/includes/header_loggedin.php
+++ application/views/includes/header_loggedin.php
@@ -14,19 +14,47 @@
 				//echo form_label('Age: '.form_input('userAge',""),'age');
     			?>
     			
-    			<div id="SB" style="background:#393939; padding: 1px;">
+    			<div id="SB" style="background:#393939; padding: 1px;
+    			display: -webkit-flex;
+    			-webkit-align-items: center;
+    			display: flex;
+    			align-items: center;">
+    			
     			<?php echo form_input('search_query', 'Search', 'id="search_bar" style="margin: 0 0 0 0; 
-    			border: none; height: 1px;width:40%; display: inline; margin-left:25%;"');
+    			border: none; height: 1px;width:30%; display: inline; margin-left:33%;"');
 				?>
-				<button type="submit" style="display: inline; margin-left: -0.5%; margin-top: -20%";" alignment-baseline:   id="search_submit"><img id="MG"src="<?php echo base_url("/images/MG.png"); ?>"/></button>				
+				<button type="submit" style="display: inline; margin-left: -0.5%;" id="search_submit"><img id="MG"src="<?php echo base_url("/images/MG.png"); ?>"/></button>				
 				</div><?php
-				echo form_close();
+				//echo form_close();
    			}?>
 		</div>
 		<div class="main_bar">
-		<?php echo anchor('catalog','Browse Catalog');?>
-		<?php echo anchor('myaccount','My account');?>
-		<?php echo anchor('home/logout','Logout!');?>
+			<div id="mb_headers">
+				<?php 
+				if($this->session->userdata('logintype')=='buyer')
+						{?>
+							<span class="electronics" id="categ_disp1" style="background: yellow;">Electronics</span>			    
+		    				<span class="furniture" id="categ_disp1" style="margin-left: 0.8%; background: red;">Furniture</span>
+					<?php } ?>
+				<div id="right">
+					<?php 
+					if($this->session->userdata('logintype')=='buyer')
+						{echo anchor('catalog','Browse Catalog','style="padding-right: 10px;"');}			
+					echo anchor('myaccount','My account','style="padding-right: 10px;"');
+					echo anchor('home/logout','Logout!','style=""');?>
+				</div>
+			</div>
+			<div id="dropdowns">
+				<ul class="electronics">
+					<li><?php echo anchor('','Cellphones','style=""');?></li>
+					<li><?php echo anchor('','Laptops','style=""');?></li>
+				</ul>
+				<ul class="furniture">
+					<li><?php echo anchor('','Table','style=""');?></li>
+					<li><?php echo anchor('','Chair','style=""');?></li>
+				</ul>
+			</div>
 		</div>
+		
 	
 	
