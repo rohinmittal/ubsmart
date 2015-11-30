c8d34ca0e96e8447a01bfd0fc122c4712fa6cccc
juggernaut-90 <vaibhavsinha90@gmail.com>
2015-11-29 19:59:29 -0500

    changing ip in config

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/ubsmart';
+#$config['base_url'] = 'http://localhost/ubsmart';
+$config['base_url'] = 'http://192.168.1.227/ubsmart';
 #$config['base_url'] = 'http://localhost/~rohinmittal/ubsmart';
 
 /*
